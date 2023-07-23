456a5b078a99675cbcd9b28e56bcacd2545f26ff
Isaac Ellwood <kingisaacellwood@gmail.com>
2023-07-24 00:05:30 +1200

    NegaMax-Aplha-Beta-Quiesce (copy pasted and doesn't work)

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -4,7 +4,6 @@ using System.Numerics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
-using static System.Formats.Asn1.AsnWriter;
 
 // This is the NegaMax Bot
 
@@ -13,74 +12,103 @@ public class MyBot : IChessBot
     public Move Think(Board board, Timer timer)
     {
         // Piece values: null, pawn, knight, bishop, rook, queen, king
-        int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000};
+        int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000, -0, -100, -300, -300, -500, -900, -10000 };
 
         List<string> chessSquares = new List<string>
+            {
+                "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
+                "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+                "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+                "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+                "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+                "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+                "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+                "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"
+            };
+        int who2move;
+        if (board.IsWhiteToMove)
         {
-            "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
-            "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
-            "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
-            "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
-            "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
-            "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
-            "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
-            "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"
-        };
+            who2move = 1;
+        }
+        else
+        {
+            who2move = -1;
+        }
 
-        // Root
-        int depth = 7;
-
-        int score = 0;
-        int hiScore = -999999999;
         Move[] moves = board.GetLegalMoves(false);
-        Random rng = new Random();
-        Move bestMove = moves[rng.Next(moves.Length)];
+        int score = 0;
+
+        // DEPTH
+        int depth = 4;
+        int max = -999999999;
+        Move bestMove = moves[moves.Length - 1];
+
         for (int i = 0; i < moves.Length; i++)
         {
             // Make move, Recursion, Undo move
             board.MakeMove(moves[i]);
-            score = alphaBetaMax(-999999999, +999999999, depth - 1);
-            if (score >= hiScore)
+            score = -negaMax(depth - 1);
+            if (score >= max)
             {
-                hiScore = score;
+                max = score;
                 bestMove = moves[i];
             }
             board.UndoMove(moves[i]);
         }
+
         return bestMove;
 
-        int alphaBetaMax(int alpha, int beta, int depthleft)
+        int negaMax(int depth)
         {
-            if (depthleft == 0) return evaluate();
+            // end of the tree
+            if (depth == 0) return evaluate();
+
+            int max = -999999999;
             Move[] moves = board.GetLegalMoves(false);
             for (int i = 0; i < moves.Length; i++)
             {
+                // Make move, Recursion, Undo move
                 board.MakeMove(moves[i]);
-                score = alphaBetaMin(alpha, beta, depthleft - 1);
+                score = -negaMax(depth - 1);
+                if (score >= max) max = score;
                 board.UndoMove(moves[i]);
+            }
+            return max;
+        }
+
+        int alphaBeta(int alpha, int beta, int depthleft)
+        {
+            if (depthleft == 0) return quiesce(alpha, beta);
+            for (all moves)
+            {
+                score = -alphaBeta(-beta, -alpha, depthleft - 1);
                 if (score >= beta)
-                    return beta;   // fail hard beta-cutoff
+                    return beta;   //  fail hard beta-cutoff
                 if (score > alpha)
                     alpha = score; // alpha acts like max in MiniMax
             }
             return alpha;
         }
 
-        int alphaBetaMin(int alpha, int beta, int depthleft)
+        int Quiesce(int alpha, int beta)
         {
-            if (depthleft == 0) return -evaluate();
-            Move[] moves = board.GetLegalMoves(false);
-            for (int i = 0; i < moves.Length; i++)
-            {
-                board.MakeMove(moves[i]);
-                score = alphaBetaMax(alpha, beta, depthleft - 1);
-                board.UndoMove(moves[i]);
-                if (score <= alpha)
-                    return alpha; // fail hard alpha-cutoff
-                if (score < beta)
-                    beta = score; // beta acts like min in MiniMax
+            int stand_pat = Evaluate();
+            if (stand_pat >= beta)
+                return beta;
+            if (alpha < stand_pat)
+                alpha = stand_pat;
+
+            until(every_capture_has_been_examined)  {
+                MakeCapture();
+                score = -Quiesce(-beta, -alpha);
+                TakeBackMove();
+
+                if (score >= beta)
+                    return beta;
+                if (score > alpha)
+                    alpha = score;
             }
-            return beta;
+            return alpha;
         }
 
         int evaluate()
@@ -101,7 +129,8 @@ public class MyBot : IChessBot
                     score += pieceValues[(int)piece.PieceType] * -1;
                 }
             }
-            return score;
+
+            return score * who2move;
         }
     }
 }
